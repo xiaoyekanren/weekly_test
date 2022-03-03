c5cfe211ce134faeb5eac5d2eb9b12da2990a0fa
liuzhen_1207 <94762806@qq.com>
2022-03-03 10:06:54 +0800

    Compaction params support 0.12 & 0.13

--- iotdb_script/iotdb_shell/set_compaction_strategy.sh
+++ iotdb_script/iotdb_shell/set_compaction_strategy.sh
@@ -1,7 +1,19 @@
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 . $DIR/env.sh
+iotdb_version=`grep "enable_seq_space_compaction=" conf/iotdb-engine.properties |wc -l`
 
-sed -i "s/.*compaction_strategy=.*/compaction_strategy=$1/g" ${iotdb_dir}/conf/iotdb-engine.properties
+if [ "${iotdb_version}" = "1" ]; then
+  if [ "$1" = "LEVEL_COMPACTION" ];then
+     v_enable="true"
+  else
+     v_enable="false"
+  fi
+  sed -i "s/.*enable_seq_space_compaction=.*/enable_seq_space_compaction=${v_enable}/g" ${iotdb_dir}/conf/iotdb-engine.properties
+  sed -i "s/.*enable_unseq_space_compaction=.*/enable_unseq_space_compaction=${v_enable}/g" ${iotdb_dir}/conf/iotdb-engine.properties
+  sed -i "s/.*enable_cross_space_compaction=.*/enable_cross_space_compaction=${v_enable}/g" ${iotdb_dir}/conf/iotdb-engine.properties
+else
+  sed -i "s/.*compaction_strategy=.*/compaction_strategy=$1/g" ${iotdb_dir}/conf/iotdb-engine.properties
+fi
 
 #备份配置文件
 
